92ee23467331b1287862e66f0aaa12c1574d210a
Boyke <boykeberryn@gmail.com>
2022-11-27 23:59:23 +0700

    added error handle for Movies

--- src/helpers/errorHandler.helpers.js
+++ src/helpers/errorHandler.helpers.js
@@ -3,12 +3,20 @@ const errorHandler = (err, res) => {
     return res.status(400).json({
       succes: false,
       message: 'Email already exist!'
-    })
+    });
+  }
+  if(err.message.includes('not present in table "movies".')){
+    if(err.message.includes('key (movieId)')){
+      return res.status(400).json({
+        succes: false,
+        message: 'movieId not found!'
+      });
+    }
   }
   return res.status(500).json({
     succes: false,
     message: 'Something happend in our backend'
-  })
+  });
 }
 
 module.exports = errorHandler
